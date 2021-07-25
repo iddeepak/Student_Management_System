c783a8b7a76a38e82f265caeac66f6a9c1babfea
iddeepak <87892182+iddeepak@users.noreply.github.com>
2021-07-25 10:19:44 +0530

    Add files via upload

---
+++ Student_Manager.h
@@ -0,0 +1,15 @@
+#include "Database_Manager.h"
+
+using namespace std;
+class StudentManager
+{
+
+public:
+    void inputData();
+    void showStudentData();
+    void showStudentDataByRollno();
+    void showResultData();
+    void showResultDataByRollno();
+    void deleteData();
+    void updateData();
+};
